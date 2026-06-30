57a88e44528d9d58087878a0b7cc2c6bb5a947a0
Merlina Ponce <merlinaponce1@gmail.com>
2026-06-29 23:15:09 -0300

    arreglos de main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-const form= document.getElementById('formularioContacto')
-const mensaje= document.getElementById('mensaje')
+const form = document.getElementById('formularioContacto');
+const mensaje = document.getElementById('mensaje');
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const nombre= document.getElementById("nombre").value.trim();
-    const email= document.getElementById("email").value.trim();
-    const mensajeTexto= document.getElementById("mensajeTexto").value.trim();
 
-    if (!nombre || !email || !mensajeTexto){
-        mensaje.textContent= "Por favor, Completá todos los campos";
-        mensaje.style.color= "red";
-    }else{
-        mensaje.textContent="Gracias por tu mensaje, en breve estaremos en contacto";
-        mensaje.style.color= "green";
+    const nombre = document.getElementById("nombre").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const mensajeTexto = document.getElementById("mensajeTexto").value.trim();
+
+    if (!nombre || !email || !mensajeTexto) {
+        mensaje.textContent = "Por favor, completá todos los campos";
+        mensaje.className = "error";
+        return;
+    } else {
+        mensaje.textContent = "Gracias por tu mensaje, en breve estaremos en contacto";
+        mensaje.className = "exito";
+        form.reset();
     }
+});
+
